610e245cfe447a4bce82fe753bc2cc3e015b023f
emrecan.demirbilek <emrecan.demirbilek@192.168.88.4>
2018-06-05 18:28:14 +0200

    sec

--- FirstJavaProject/src/Homework31Horse.java
+++ FirstJavaProject/src/Homework31Horse.java
@@ -1,4 +1,14 @@
+import java.util.Random;
 
 public class Homework31Horse {
 
+public static void main (String [] args) {
+	
+	Random nbr = new Random ();
+			nbr random.nextInt;
+	
+	
+	int[ ] arr = new int[5];
+}
+	
 }
