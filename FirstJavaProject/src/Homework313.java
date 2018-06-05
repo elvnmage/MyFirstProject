ae034d417ceff333adeb6f97f1a84684f528ecab
edemirbilek <edemirbilek@10.0.1.47>
2018-06-06 00:20:05 +0200

    bla

--- FirstJavaProject/src/Homework313.java
+++ FirstJavaProject/src/Homework313.java
@@ -1,4 +1,52 @@
+//import java.util.Random;
 
-public class Homework313 {
+public class Homework313
 
+{
+	
+    public static void main (String [] args) 
+    
+    {
+    	
+ 
+    		int a[]=new int[5];//declaration and instantiation  
+    		a[0]=10;//initialization  
+    		a[1]=20;  
+    		a[2]=70;  
+    		a[3]=40;  
+    		a[4]=50;  
+    		  
+    		//printing array  
+    		for(int i=0;i<a.length;i++)//length is the property of array  
+    		System.out.println(a[i]);  
+    		
+    
+    		     // int myList = 19;
+
+    		      // Print all the array elements
+    		   //   for(int i = 1; i <= size; i++)  {
+    		   //      System.out.println(element);
+    		 //     }
+       // int size = 150;
+
+      //  int arr [] = new int[5];
+     //   for(int i = 1; i <= size; i++) 
+        
+     //   {
+           
+            
+     //   }
+
+        
+   //     Random rand = new Random();
+    //    while(list.size() > 0) 
+        
+    //    {
+    //        int index = rand.nextInt(list.size());
+     //       System.out.println("Random Number: "+list.remove(index));
+            
+      //  }
+   
+    }
+    
 }
--- FirstJavaProject/src/Homework31Horse.java
+++ FirstJavaProject/src/Homework31Horse.java
@@ -1,14 +1,22 @@
-import java.util.Random;
+//import java.util.Random;
 
 public class Homework31Horse {
 
 public static void main (String [] args) {
 	
-	Random nbr = new Random ();
-			nbr random.nextInt;
+	int a[]=new int[5];//declaration and instantiation  
+	a[0]=10;//initialization  
+	a[1]=20;  
+	a[2]=70;  
+	a[3]=40;  
+	a[4]=50;  
+	  
+	//printing array  
+	for(int i=0;i<a.length;i++)//length is the property of array  
+	System.out.println(a[i]);
 	
 	
-	int[ ] arr = new int[5];
+//	int[ ] arr = new int[5];
 }
 	
 }
